73997892f7c7a63dee67841de9fb52c1776de6ef
Rasputin <dasturboboot@gmail.com>
2024-08-09 19:03:36 -0500

    added main.sh

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-print("Hello world")
+from textnode import TextNode
+
+def main():
+	node = TextNode("This is a text node", "bold", "https://www.boot.dev")
+	print(node)
+
+if __name__ == "__main__":
+    main()
